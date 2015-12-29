4bfb905ff4619f38f7695f4bdac3ea7a7c802436
poplanchong123 <1227191457@qq.com>
2015-12-29 13:30:43 +0800

    first

---
+++ JqwidgetsMVCDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,95 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace JqwidgetsMVCDemo
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqwidgets").Include(
+            "~/Scripts/jqxcore.js",
+            "~/Scripts/jqxdata.js",
+            "~/Scripts/jqxgrid.js",
+            "~/Scripts/jqxgrid.selection.js",
+            "~/Scripts/jqxgrid.pager.js",
+            "~/Scripts/jqxlistbox.js",
+            "~/Scripts/jqxbuttons.js",
+            "~/Scripts/jqxscrollbar.js",
+            "~/Scripts/jqxdatatable.js",
+            "~/Scripts/jqxtreegrid.js",
+            "~/Scripts/jqxmenu.js",
+            "~/Scripts/jqxcalendar.js",
+            "~/Scripts/jqxgrid.sort.js",
+            "~/Scripts/jqxgrid.filter.js",
+            "~/Scripts/jqxdatetimeinput.js",
+            "~/Scripts/jqxdropdownlist.js",
+            "~/Scripts/jqxslider.js",
+            "~/Scripts/jqxeditor.js",
+            "~/Scripts/jqxinput.js",
+            "~/Scripts/jqxdraw.js",
+            "~/Scripts/jqxchart.core.js",
+            "~/Scripts/jqxchart.rangeselector.js",
+            "~/Scripts/jqxtree.js",
+            "~/Scripts/globalize.js",
+            "~/Scripts/jqxbulletchart.js",
+            "~/Scripts/jqxcheckbox.js",
+            "~/Scripts/jqxradiobutton.js",
+            "~/Scripts/jqxvalidator.js",
+            "~/Scripts/jqxpanel.js",
+            "~/Scripts/jqxpasswordinput.js",
+            "~/Scripts/jqxnumberinput.js",
+            "~/Scripts/jqxcombobox.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/jqx.base.css",
+                "~/Content/jqx.arctic.css",
+                "~/Content/jqx.black.css",
+                "~/Content/jqx.bootstrap.css",
+                "~/Content/jqx.classic.css",
+                "~/Content/jqx.darkblue.css",
+                "~/Content/jqx.energyblue.css",
+                "~/Content/jqx.fresh.css",
+                "~/Content/jqx.highcontrast.css",
+                "~/Content/jqx.metro.css",
+                "~/Content/jqx.metrodark.css",
+                "~/Content/jqx.office.css",
+                "~/Content/jqx.orange.css",
+                "~/Content/jqx.shinyblack.css",
+                "~/Content/jqx.summer.css",
+                "~/Content/jqx.web.css",
+                "~/Content/jqx.ui-darkness.css",
+                "~/Content/jqx.ui-lightness.css",
+                "~/Content/jqx.ui-le-frog.css",
+                "~/Content/jqx.ui-overcast.css",
+                "~/Content/jqx.ui-redmond.css",
+                "~/Content/jqx.ui-smoothness.css",
+                "~/Content/jqx.ui-start.css",
+                "~/Content/jqx.ui-sunny.css",
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"
+                ));
+        }
+    }
+}
---
+++ JqwidgetsMVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JqwidgetsMVCDemo.Models;
+
+namespace JqwidgetsMVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //http://www.jqwidgets.com/jquery-widgets-demo/demos/aspnetmvc/index.htm?(arctic)
+
+        private EmployeeDBEntities db = new EmployeeDBEntities();
+
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: /Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: /Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="EmployeeID,ManagerID,FirstName,LastName,Title,Country,City,Address,HireDate,BirthDate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: /Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: /Employee/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="EmployeeID,ManagerID,FirstName,LastName,Title,Country,City,Address,HireDate,BirthDate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: /Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: /Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public JsonResult GetEmployees()
+        {
+            var dbResult = db.Employees.ToList();
+            var employees = (from employee in dbResult
+                             select new
+                             {
+                                 employee.FirstName,
+                                 employee.LastName,
+                                 employee.EmployeeID,
+                                 employee.BirthDate,
+                                 employee.HireDate,
+                                 employee.ManagerID,
+                                 employee.Title,
+                                 employee.City,
+                                 employee.Country,
+                                 employee.Address
+                             });
+            return Json(employees, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
